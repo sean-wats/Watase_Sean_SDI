e90118d4c9ef49ba2bbbc4279e9bf5ba1cce97d9
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-01 14:48:17 -1000

    Assigned a new String variable
    Assigned a new variable called itemDescription to a value describing
    the item.

--- Watase_Sean_Output_Assignment/Watase_Sean_Ouput_Assingment.js
+++ Watase_Sean_Output_Assignment/Watase_Sean_Ouput_Assingment.js
@@ -9,6 +9,8 @@
 // Assigning a String variable
 var itemName = "Water Bottle";
 
+var itemDescription = "This is a insulated water bottle to keep your liquid cold/hot for long period of time.";
+
 // Assigning a Number variable
 var itemPrice = 34.99;
 
