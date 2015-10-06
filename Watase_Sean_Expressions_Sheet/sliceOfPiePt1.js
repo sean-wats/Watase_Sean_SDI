d674dbde479d17f2a1ecec00834d52b9e7d406e1
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-06 08:47:53 -1000

    Commented the steps to take in problem 2
    Added comments to the javascript file for problem 2 on what we will be
    doing to accomplish the final result.

--- Watase_Sean_Expressions_Sheet/sliceOfPiePt1.js
+++ Watase_Sean_Expressions_Sheet/sliceOfPiePt1.js
@@ -1,1 +1,9 @@
+// Slices of Pie Part 1
+
+//Finding out how many slices of pizza each person will get
+
+//User answer 3 prompts:
+    //How many slices per pizza
+    //How many people at the party
+    //How many pizzas were ordered
 
