924e224852ed1d36772c4b73f89b6b3d5f583d0b
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-05 22:38:27 -1000

    Problem 1 layout
    Commented the javascript file for problem 1. Added what the problem is
    and how the program will work.

--- Watase_Sean_Expressions_Sheet/dogYears.js
+++ Watase_Sean_Expressions_Sheet/dogYears.js
@@ -1,1 +1,7 @@
+//Dog Years
 
+//Convert dog years into human years (Age)
+//7 dog years = 1 human years
+
+//User will enter the age of Sparky the dog in human years.
+//Will return Sparky's age in dog years.
