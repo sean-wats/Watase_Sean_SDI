f63ef533d1126e3c5b02db81c93e12f549c357da
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-06 17:30:35 -1000

    Added comment for objective
    Added the comments to what will need to be done in the file for problem
    4.

--- Watase_Sean_Expressions_Sheet/discounts.js
+++ Watase_Sean_Expressions_Sheet/discounts.js
@@ -1,1 +1,10 @@
 //Discount Problem
+
+//Get original price
+//Get discount amount
+//Description of item
+//Sales tax amount
+
+//Will return the original amount and amount with discount and no tax along with amount with discount and tax.
+
+
