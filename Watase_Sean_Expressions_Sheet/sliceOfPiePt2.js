b30add689035f7c466cfa052046b21c17e17d82d
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-06 17:13:57 -1000

    Added files for problem 3
    Copy and pasted file from problem 2 since problem 3 is going further
    with what we already did.

---
+++ Watase_Sean_Expressions_Sheet/sliceOfPiePt2.js
@@ -0,0 +1,30 @@
+// Slices of Pie Part 1
+
+//Finding out how many slices of pizza each person will get
+
+//User answer 3 prompts:
+    //How many slices per pizza
+    //How many people at the party
+    //How many pizzas were ordered
+
+//User will answer these prompts
+
+//Slices per pizza
+var slicesPerPizza = prompt("How many slices does the pizza have?");
+
+//People at the party
+var numberOfPeopleAtParty = prompt("How many people are at the party?");
+
+//Number of pizzas ordered
+var numberOfPizzasOrdered = prompt("How many pizzas did you order?");
+
+
+//Solving how many slices each person will get
+var numberOfSlicesPerPerson = (numberOfPizzasOrdered * slicesPerPizza) / numberOfPeopleAtParty;
+
+
+//Outputting result to console and user
+var finalResult = "If you order " + numberOfPizzasOrdered + " pizzas and each pizza has " + slicesPerPizza + " slices. Each of the " + numberOfPeopleAtParty + " people at the party will get " + numberOfSlicesPerPerson + " slices.";
+
+console.log(finalResult);
+alert(finalResult);
