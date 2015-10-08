81592650c5020daf25e56750bdb4a1bdda03cbe6
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-07 22:25:52 -1000

    Created array values
    Prompt to user will be saved as an array value to be used in expression
    when needed.

--- Watase_Sean_Expression_Assignment/expessionAssignment.js
+++ Watase_Sean_Expression_Assignment/expessionAssignment.js
@@ -3,3 +3,13 @@
 //October 7,2015
 
 //Finding the average of 3 numbers
+
+//Creating an array for the 3 numbers
+var numberArray = []
+
+//User input for 3 numbers
+numberArray[0] = prompt("We will be finding the sum of 3 numbers. \nWhat is your first number?");
+
+numberArray[1] = prompt("What is your second number?");
+
+numberArray[2] = prompt("What is your third number?");
