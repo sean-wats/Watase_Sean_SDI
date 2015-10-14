43c3f9c7aea60b3920134058ddbea0f54903740d
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 12:53:52 -1000

    Wrote code to accept user input
    Used prompt to collect the input of the user. (What temperature they
    want to convert).

---
+++ Watase_Sean_Conditionals_Worksheet/js/problem1.js
@@ -0,0 +1,7 @@
+//Conditionals Worksheet
+
+//Problem 1 - Temperature converter
+//Converts temperature depending on the temperature user puts in
+
+//Getting user input on what temperature they want to convert
+var userTemperture = prompt("What is the temperature you want to convert? \n(End temperature in F for fahrenheit or C for celsius)");
