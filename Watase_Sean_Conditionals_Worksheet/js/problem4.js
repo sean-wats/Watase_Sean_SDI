8c679cce82c8a94c23d3910e06d0dc6a1baf05dd
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 23:37:29 -1000

    Complete output
    Completed the output of what alert will show depending if the username
    and password are correct or not.

--- Watase_Sean_Conditionals_Worksheet/js/problem4.js
+++ Watase_Sean_Conditionals_Worksheet/js/problem4.js
@@ -15,3 +15,17 @@ var password = "password";
 //User will input user name and password
 var userNameEntered = prompt("Username:");
 var passwordEntered = prompt("Passowrd:");
+
+console.log(userNameEntered);
+console.log(passwordEntered);
+
+//Validate if user name and password match
+if (userNameEntered === userName && passwordEntered === password) {
+    alert("Welcome " + userName + "!");
+}
+else if (userNameEntered === userName && passwordEntered != password) {
+    alert("Password does not match what we have on record.");
+}
+else {
+    alert("User not found. Try again");
+}
