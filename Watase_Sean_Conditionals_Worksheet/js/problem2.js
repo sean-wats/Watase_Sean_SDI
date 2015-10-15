2f6164d346e3c1df2c8557eea27e1ca215695e66
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 18:10:50 -1000

    Inputs for user created
    Created all the prompts needed to gather info from user.

--- Watase_Sean_Conditionals_Worksheet/js/problem2.js
+++ Watase_Sean_Conditionals_Worksheet/js/problem2.js
@@ -5,3 +5,7 @@
 //They are passing the last gas station for 200miles
 
 //Must get users gas efficiency, gauge reading, and cars tank capacity
+var gasEfficiency = prompt("What is your cars MPG(Miles Per Gallon)?");
+var gaugeReading = prompt("At what percent is your gas tank gauge reading?");
+var carTankCapacity = prompt("How much gallons of gas can your tank hold?");
+
