3344c84f6efb322f660f56807e58eec8c6ebb4a3
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 09:26:15 -1000

    Created conditionals
    Created the prompts to gather user info and created a conditional to
    see what the ticket price for the customers ticket would be.

--- Watase_Sean_Conditionals_Worksheet/js/problem6.js
+++ Watase_Sean_Conditionals_Worksheet/js/problem6.js
@@ -11,4 +11,21 @@
  * Between 3 - 5    $7.00
  **/
 
+//Ticket Prices
+    var ticketPrice = "Your ticket will be $12.00";
+    var discountTicketPrice = "Your ticket will be $7.00";
+
 //Find out what time the movie is at and how old the customer is
+var movieTime = prompt("What tie is your movie at?");
+var customerAge = prompt("How old are you?");
+
+//Conditional to see what price they need to pay
+if (movieTime <= 5 && movieTime >= 3) {
+    alert(discountTicketPrice);
+}
+else if (customerAge >= 55 || customerAge <= 10) {
+    alert(discountTicketPrice);
+}
+else {
+    alert(ticketPrice);
+}
