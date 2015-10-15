1717f6d2f4dc97f704304a20395c4ab6d9f0c2a0
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 18:17:56 -1000

    Alerts created
    Alerts were created to let the user know if they can make it to the
    next gas station or not.

--- Watase_Sean_Conditionals_Worksheet/js/problem2.js
+++ Watase_Sean_Conditionals_Worksheet/js/problem2.js
@@ -5,7 +5,25 @@
 //They are passing the last gas station for 200miles
 
 //Must get users gas efficiency, gauge reading, and cars tank capacity
-var gasEfficiency = prompt("What is your cars MPG(Miles Per Gallon)?");
-var gaugeReading = prompt("At what percent is your gas tank gauge reading?");
-var carTankCapacity = prompt("How much gallons of gas can your tank hold?");
+var gasEfficiencyInput = prompt("What is your cars MPG(Miles Per Gallon)?");
+var gaugeReadingInput = prompt("At what percent is your gas tank gauge reading?");
+var carTankCapacityInput = prompt("How much gallons of gas can your tank hold?");
 
+//Converting Stings to Numbers
+var gasEfficiency = Number(gasEfficiencyInput);
+var gaugeReading = Number(gaugeReadingInput);
+var carTankCapacity = Number(carTankCapacityInput);
+
+//Calculating how much gas they have left
+var remainingGas = (gaugeReading / 100) * carTankCapacity;
+//Calculating how much miles can be traveled with remaining gas
+var travelDistanceWithRemainingGas = gasEfficiency * remainingGas;
+console.log(travelDistanceWithRemainingGas);
+
+//Finding if remainingGas is greater than 200
+if (travelDistanceWithRemainingGas >= 200) {
+    alert("You have enough gas to reach the next gas station")
+}
+else {
+    alert("Fill up gas now!");
+}
