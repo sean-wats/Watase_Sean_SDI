6f1396763b45cf5933738bb569d4717424c078fb
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 09:11:31 -1000

    Created problem 5 files
    Created the files needed for problem 5 and added the objectives that
    needed to be done.

---
+++ Watase_Sean_Conditionals_Worksheet/js/problem5.js
@@ -0,0 +1,7 @@
+//Problem 5
+//Tire Pressure
+
+//Front tires must have same tire pressure
+//Back tires must have the same tire pressure
+//Front set and back set can have different tire pressures
+
