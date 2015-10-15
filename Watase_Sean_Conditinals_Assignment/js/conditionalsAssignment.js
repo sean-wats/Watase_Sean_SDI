51b572383f49d77f115b1e6fe66ab1ea6e45ef0f
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 09:51:10 -1000

    Creating income calculation
    Created code to calculate the total income amount of the user. This way
    we can figure out what there budget will look like.

--- Watase_Sean_Conditinals_Assignment/js/conditionalsAssignment.js
+++ Watase_Sean_Conditinals_Assignment/js/conditionalsAssignment.js
@@ -2,4 +2,23 @@
 //Budget Calculator
 
 //Get the paycheck amounts
+var paycheck = Number(prompt("How much did you receive on your paycheck?"));
 
+var additionalPaychecks = prompt("Do you have any additional paychecks you want to add? \n(Yes or No)");
+
+if (additionalPaychecks === "Yes") {
+    var addPaycheck = Number(prompt("What is the amount of the paycheck?"));
+
+    var paycheckTotal = paycheck + addPaycheck;
+    console.log("Income total is: " + paycheckTotal);
+}
+else if (additionalPaychecks === "No") {
+    
+}
+
+/**
+ * Budget will consist of:
+ * Bills
+ * Food
+ * Savings
+ */
