c30fb7655cde953e7a54525091bb1f71e4a282e6
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 10:42:10 -1000

    Created Validation
    Created a condition to validate that the information being inputted is
    valid. We also logged the remaining amount of the users budget in
    savings and misc.

--- Watase_Sean_Conditinals_Assignment/js/conditionalsAssignment.js
+++ Watase_Sean_Conditinals_Assignment/js/conditionalsAssignment.js
@@ -8,6 +8,11 @@
  * Savings
  */
 
+//Invalid alert
+var invalid = "The information you have provided is not valid.";
+
+
+
 //Get the paycheck amounts
 var paycheck = Number(prompt("How much did you receive on your paycheck?"));
 
@@ -26,10 +31,36 @@ if (additionalPaychecks == "Yes") {
     var totalBills = Number(prompt("What is the total amount of the bills you pay? \n(For the month)"));
     var totalFood = Number(prompt("How much do you spend on food? \n(For the month)"));
 
+    //Total of spending budget
     var totalSpendingBudget = totalBills + totalFood;
+    console.log("Your total spending amount is: " + totalSpendingBudget);
 
     //Finding total amount of the user savings (Remaining after the spending budget)
-    var savingsAmount = paycheckTotal - totalSpendingBudget;
+    var remainingAmount = paycheckTotal - totalSpendingBudget;
+    console.log("Your remaining balance after spending budget is: " + remainingAmount);
+
+    //Validating that the savings amount is greater than 0
+    if (remainingAmount >= 0) {
+        //Seeing if user wants to put amount away in savings
+        var savingsAmount = Number(prompt("What percentage would you like to put away and save? \n(Value must be between 0 - 100)"));
+
+        //Validating to see if percentage amount is valid
+        if (savingsAmount >= 0 && savingsAmount <= 100) {
+            var savingsAmountPercentage = savingsAmount / 100;
+
+            //Finding the amount that will go to savings and the remaining is for users misc purposes
+            var userSavings = remainingAmount * savingsAmountPercentage;
+            var userMisc = remainingAmount - userSavings;
+            console.log(userSavings);
+            console.log(userMisc);
+        }
+        else {
+            alert(invalid);
+        }
+    }
+    else {
+        alert("You have no remaining balance after your spending budget. Your spending budget exceeds your income.");
+    }
 }
 else if (additionalPaychecks == "No") {
     console.log("Income total is: " + paycheck);
@@ -40,11 +71,36 @@ else if (additionalPaychecks == "No") {
     var totalBills = Number(prompt("What is the total amount of the bills you pay? \n(For the month)"));
     var totalFood = Number(prompt("How much do you spend on food? \n(For the month)"));
 
+    //Total of spending budget
     var totalSpendingBudget = totalBills + totalFood;
+    console.log("Your total spending amount is: " + totalSpendingBudget);
 
     //Finding total amount of the user savings (Remaining after the spending budget)
-    var savingsAmount = paycheck - totalSpendingBudget;
+    var remainingAmount = paycheck - totalSpendingBudget;
+
+    //Validating that the savings amount is greater than 0
+    if (remainingAmount >= 0) {
+        //Seeing if user wants to put amount away in savings
+        var savingsAmount = Number(prompt("What percentage would you like to put away and save? \n(Value must be between 0 - 100)"));
+
+        //Validating to see if percentage amount is valid
+        if (savingsAmount >= 0 && savingsAmount <= 100) {
+            var savingsAmountPercentage = savingsAmount / 100;
+
+            //Finding the amount that will go to savings and the remaining is for users misc purposes
+            var userSavings = remainingAmount * savingsAmountPercentage;
+            var userMisc = remainingAmount - userSavings;
+            console.log(userSavings);
+            console.log(userMisc);
+        }
+        else {
+            alert(invalid);
+        }
+    }
+    else {
+        alert("You have no remaining balance after your spending budget. Your spending budget exceeds your income.");
+    }
 }
 else {
-    alert("The information you have provided is not valid.");
+    alert(invalid);
 }
