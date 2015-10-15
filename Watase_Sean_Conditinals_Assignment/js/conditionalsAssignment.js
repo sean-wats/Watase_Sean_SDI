9e47b571a07e8ed0d449667e4b06090864610630
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 10:03:29 -1000

    Created total spending budget
    Created the code to gather the total amount of the spending budget of
    the user.

--- Watase_Sean_Conditinals_Assignment/js/conditionalsAssignment.js
+++ Watase_Sean_Conditinals_Assignment/js/conditionalsAssignment.js
@@ -9,6 +9,7 @@ var additionalPaychecks = prompt("Do you have any additional paychecks you want 
 if (additionalPaychecks === "Yes") {
     var addPaycheck = Number(prompt("What is the amount of the paycheck?"));
 
+    //Adding both paychecks together to get total
     var paycheckTotal = paycheck + addPaycheck;
     console.log("Income total is: " + paycheckTotal);
 
@@ -20,11 +21,14 @@ else if (additionalPaychecks === "No") {
     alert("Your total income is: " + paycheck);
 }
 
-
-
 /**
  * Budget will consist of:
  * Bills
  * Food
  * Savings
  */
+//Finding total amount of user spending budget (Bills and food)
+var totalBills = Number(prompt("What is the total amount of the bills you pay? \n(For the month)"));
+var totalFood = Number(prompt("How much do you spend on food? \n(Per month)"));
+
+var totalSpendingBudget = totalBills + totalFood;
