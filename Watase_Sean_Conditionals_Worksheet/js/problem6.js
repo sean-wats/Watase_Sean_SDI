97b32ceef726e586a3ae40ac92b0697c6f671710
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 09:20:45 -1000

    Created files for problem 6
    Created the files need for problem 6 and created the objectives that
    need tho be done.

---
+++ Watase_Sean_Conditionals_Worksheet/js/problem6.js
@@ -0,0 +1,14 @@
+//Problem 6
+//Movie Ticket price
+
+//Find out which movie ticket price the customer will pay
+
+/**
+ * Ticket prices:
+ * Regular          $12.00
+ * Senior           $7.00
+ * Under 10         $7.00
+ * Between 3 - 5    $7.00
+ **/
+
+//Find out what time the movie is at and how old the customer is
