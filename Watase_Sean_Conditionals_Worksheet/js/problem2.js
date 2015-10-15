f24c2b519271fcacd9f55770f505e372cba901ab
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 18:08:02 -1000

    Created problem 2 and objectives
    Created files and objectives needed to create the code for problem 2

---
+++ Watase_Sean_Conditionals_Worksheet/js/problem2.js
@@ -0,0 +1,7 @@
+//Problem 2
+//Last chance for gas
+
+//Determine if the user has enough gas to get them home
+//They are passing the last gas station for 200miles
+
+//Must get users gas efficiency, gauge reading, and cars tank capacity
