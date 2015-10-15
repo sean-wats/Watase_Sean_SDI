ad8beeddf0a92c714e0846f5f4c732b9a794eff4
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 18:26:05 -1000

    Problem 3 objective stated
    Created files and objective for problem 3 code.

---
+++ Watase_Sean_Conditionals_Worksheet/js/problem3.js
@@ -0,0 +1,17 @@
+//Problem 3
+//Letter Grade
+
+//Show the right letter grade for the percentage they are receiving in class
+/**
+ * Full Sail Grading Scale
+ *
+ *  A+ = 95-100
+ *  A = 90-94
+ *  B+ = 85-89
+ *  B = 80-84
+ *  C+ = 76-79
+ *  C = 73-75
+ *  D = 70-72
+ *  F = 0-69
+ **/
+
