f5c57e5f1c0cd869d0571d7a8f146b1f25b63536
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-14 23:30:23 -1000

    Created problem 4
    Created the files needed and the solution for problem 4.

---
+++ Watase_Sean_Conditionals_Worksheet/js/problem4.js
@@ -0,0 +1,14 @@
+//Problem 4
+//User name and password
+
+//User inputs user name and password
+
+/**
+ * For this code example:
+ * user name will be seanwatase
+ * and password will be password
+ */
+
+
+
+//User will input user name and password
