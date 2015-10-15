992a1844ee53c830da83bfb5774674b7bbc2266b
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-15 09:15:50 -1000

    Created the code
    Created the code to gather the tire pressure of all four tires and used
    a conditional the figure out if the car passed the test or not.

--- Watase_Sean_Conditionals_Worksheet/js/problem5.js
+++ Watase_Sean_Conditionals_Worksheet/js/problem5.js
@@ -5,3 +5,17 @@
 //Back tires must have the same tire pressure
 //Front set and back set can have different tire pressures
 
+//Gathering the tire pressure of the four tires
+var frontLeft = prompt("Front left tire pressure:");
+var frontRight = prompt("Front right tire pressure:");
+
+var backLeft = prompt("Back left tire pressure:");
+var backRight = prompt("Back right tire pressure");
+
+//Creating one conditional to see if car will pass test
+if (frontLeft === frontRight && backLeft === backRight) {
+    alert("Car passed tire test.");
+}
+else {
+    alert("Car did not pass tire test. Must get tire checked.");
+}
