05d7cd9d663aae2a81f2ca6dd524ef7f4583de6d
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-20 17:42:25 -1000

    Solving problem 1
    Created code for problem 1. Still needs validation on input.

--- Watase_Sean_Functions_Worksheet/js/problem1.js
+++ Watase_Sean_Functions_Worksheet/js/problem1.js
@@ -2,3 +2,22 @@
 //Problem 1
 
 //Solving Circumference
+//Circumference = 2 * (3.14) * radius
+
+alert("Circumference Calculator");
+
+var calculateCircumference =  function(radius){
+    if (radius === "") {
+        alert("Invalid Value");
+    }
+    else {
+        var circumference = 2 * 3.14 * radius;
+    }
+
+    console.log(circumference);
+    return (circumference);
+};
+
+var answer = calculateCircumference(Number(prompt("What is the radius?")));
+
+alert("The circumference is " + answer);
