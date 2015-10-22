d1f49c83b46357e4bec099caa54bdfca43d085bd
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-21 21:40:30 -1000

    5 White Balls
    Created the code to draw 5 random balls between the values of 1 and 69.

--- Watase_Sean_Functions_Assignment/js/functionsAssignment.js
+++ Watase_Sean_Functions_Assignment/js/functionsAssignment.js
@@ -9,6 +9,18 @@
 
 //5 whites balls must be drawn
 //Cannot have duplicates
+var ballNumber = 1;
+
+do {
+    var randomNumber = function() {
+        return Math.round(Math.random() * (69 - 1)) + 1;
+    };
+    var randomBallNumber = randomNumber();
+    console.log(randomBallNumber);
+
+    ballNumber ++;
+}
+while (ballNumber < 6);
 
 //1 red ball must be drawn
 
