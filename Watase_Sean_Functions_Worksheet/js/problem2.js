aab0a601493a3e210c80484a62fce52cac92eb2f
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-21 15:52:40 -1000

    Finish Problem 2 code
    Created the code for problem 2. Along with verification to make sure
    that the user has inputted a number value and a value. Calculator will
    determine the amount of stings it takes to kill the victim.

--- Watase_Sean_Functions_Worksheet/js/problem2.js
+++ Watase_Sean_Functions_Worksheet/js/problem2.js
@@ -4,3 +4,28 @@
 //Bee Stings
 //It takes 8.666666667 stings per pound to kill it victim
 
+//Name of the code
+alert("Bee Sting Calculator");
+
+var beeStingCalculator = function(victimsWeight) {
+  if (victimsWeight === "") {
+      alert("Invalid Value");
+  }
+    else {
+      var numberOfBeeStingsToKill = victimsWeight * 8.666666667;
+
+      var numberVerification = isNaN(numberOfBeeStingsToKill);
+
+      if (numberVerification === false) {
+          console.log(numberOfBeeStingsToKill);
+          return (numberOfBeeStingsToKill);
+      }
+      else {
+          alert("Invalid Value");
+      }
+  }
+};
+
+var answer = beeStingCalculator(Number(prompt("What is the victims weight? \n(In pounds(lbs.))")));
+
+alert("It would take " + answer + " stings to kill the victim.");
