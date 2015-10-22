97921cb88cafe96d67c555fae7f9828788137f3b
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-21 21:22:27 -1000

    Functions Assignment
    Created the files needed to create the functions assignment. For the
    assignment I will be doing the powerball drawing. I have laid out
    comments to describe the task that must be done.

---
+++ Watase_Sean_Functions_Assignment/js/functionsAssignment.js
@@ -0,0 +1,15 @@
+//Functions Assignment
+//Power Ball
+
+/**
+ * Power Ball Rules
+ * 5 white balls are drawn (1-69)
+ * 1 red ball is drawn (1-25)
+ */
+
+//5 whites balls must be drawn
+//Cannot have duplicates
+
+//1 red ball must be drawn
+
+//Final result is 5 white balls and 1 red ball
