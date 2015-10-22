05cbc063fe1d32acd7a32cc145d03d6611226068
sean-wats <seanwatase@Seans-MacBook-Pro.local>
2015-10-22 10:17:58 -1000

    Created Drawing Ouput
    Created the final drawing output where the user is alerted the final
    draws along with the power ball.

--- Watase_Sean_Functions_Assignment/js/functionsAssignment.js
+++ Watase_Sean_Functions_Assignment/js/functionsAssignment.js
@@ -11,15 +11,14 @@
 var ballNumber = 0;
 var whiteBalls = [];
 
+//Random Number function
+var randomNumber = function() {
+    return Math.round(Math.random() * (69 - 1)) + 1;
+};
+
 var drawingWhiteBalls = function() {
     do {
-        var randomNumber = function () {
-            return Math.round(Math.random() * (69 - 1)) + 1;
-        };
-        var randomBallNumber = randomNumber();
-        console.log(randomBallNumber);
-
-        whiteBalls[ballNumber] = randomBallNumber;
+        whiteBalls[ballNumber] = randomNumber();
 
         ballNumber++;
     }
@@ -28,13 +27,31 @@ var drawingWhiteBalls = function() {
 drawingWhiteBalls();
 
 //Cannot have duplicates
-if (ballNumber[0] === ballNumber[1] || ballNumber[0] === ballNumber[2] || ballNumber[0] === ballNumber[3] || ballNumber[0] === ballNumber [4] || ballNumber[1] === ballNumber[2] || ballNumber[1] === ballNumber[3] || ballNumber[1] === ballNumber[4] || ballNumber[2] === ballNumber[3] || ballNumber[2] === ballNumber[4] || ballNumber[3] === ballNumber[4]) {
-    drawingWhiteBalls();
-}
-else {
-    
-}
+//Redraw ball 1 if it is the same number as any other ball
+
+/**
+ * Outputting the final draw
+ * All 5 white balls should be different numbers
+ */
+console.log("The white balls are " + whiteBalls);
+
 
 //1 red ball must be drawn
+var randomRedBall = function() {
+    return Math.round(Math.random() * (25-1) + 1);
+};
+
+var drawingRedBall = randomRedBall();
+
+/**
+ * Outputting the final Power Ball
+ * 1 Power Ball from 1-25
+ */
+console.log("The Power Ball is " + drawingRedBall);
 
 //Final result is 5 white balls and 1 red ball
+alert("Ball 1: " + whiteBalls[0] +
+      "\nBall 2: " + whiteBalls[1] +
+      "\nBall 3: " + whiteBalls[2] +
+      "\nBall 4: " + whiteBalls[3] +
+      "\nBall 5: " + whiteBalls[4])
